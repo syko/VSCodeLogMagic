b8df65cede2aa5a3dbbd4ed1b2593ef86a0cf02f
syko <aa.andrin@gmail.com>
2021-12-09 14:13:34 +0200

    Initial test setup & lint config

---
+++ src/test/logger.test.ts
@@ -0,0 +1,9 @@
+import * as assert from 'assert';
+
+suite('Javascript Test Suite', () => {
+
+	test('Sample test', () => {
+		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
+		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+	});
+});
