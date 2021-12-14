7564f7877e568024388d161715f60da37a389f76
syko <aa.andrin@gmail.com>
2021-12-14 18:06:00 +0200

    Enable warnings for unused variables

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,7 +24,7 @@ module.exports = {
   },
   rules: {
     'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-unused-vars': 'warn',
     'no-param-reassign': 'off',
     'prefer-template': 'off',
     'no-mixed-operators': 'off',
